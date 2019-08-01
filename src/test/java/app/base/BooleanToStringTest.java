56b9c6b1febcd66c9b082a3d13772cdb68bd7b0a
huanqingdong <609139528@qq.com>
2019-08-01 22:16:15 +0800

    7.28

---
+++ src/test/java/app/base/BooleanToStringTest.java
@@ -0,0 +1,15 @@
+package app.base;
+
+import org.junit.Test;
+
+/**
+ * @author faith.huan 2019-07-28 20:06
+ */
+public class BooleanToStringTest {
+
+    @Test
+    public void test(){
+        boolean b = true;
+        System.out.println(String.valueOf(true));
+    }
+}
